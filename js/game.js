1a6b7a4f70aa92e6f5fe6f9244d54565e1459cd3
Claire Lacanal <lacanal.claire@gmail.com>
2025-03-20 11:27:53 +0100

    delete coms

--- js/game.js
+++ js/game.js
@@ -1,3 +1,40 @@
 class Game {
-    // code to be added
-}
+    constructor(){
+        this.startScreen = document.querySelector("#game-intro");
+        this.gameScreen = document.querySelector("#game-screen");
+        this.gameEndScreen = document.querySelector("#game-end");
+        this.player = null;
+        this.height = 600;
+        this.width = 500;
+        this.obstacles = [];
+        this.score = 0;
+        this.lives = 3;
+        this.gameIsOver = false;
+        this.gameIntervalId;
+        this.gameLoopFrecuency = Math.round(1000/60);
+    }
+    start(){
+        this.gameScreen.style.height = `${this.height}px`;
+        this.gameScreen.style.width = `${this.width}px`;
+        
+        this.startScreen.display = "none";
+
+        this.startScreen.display = ""
+        let i = 0;
+        const intervalId = setInterval(function () {
+            i++;
+
+            if(i > 60){
+                gameLoop();
+                clearInterval(intervalId);
+            }
+        }, gameLoopFrecuency)
+
+    }
+    gameLoop(){
+        if(gameIsOver === true){
+            setInterval(gameIntervalId);
+        }
+    }
+    
+
